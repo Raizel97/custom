63da508e5da4edf833ff2a7a50abb18b785898b2
Raizel97 <mohammad.fekry97@gmail.com>
2016-10-17 18:42:10 +0200

    Update and rename extension.js to havokbot.js

--- havokbot.js
+++ havokbot.js
@@ -1,7 +1,7 @@
 (function () {
 
     // Change this to your GitHub username so you don't have to modify so many things.
-    var fork = "basicBot";
+    var fork = "Raizel97";
 
     // Define our function responsible for extending the bot.
     function extend() {
@@ -46,19 +46,19 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "basicBot",
+      botName: "HavokBot",
       language: "english",
       chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
-      startupCap: 1, // 1-200
-      startupVolume: 0, // 0-100
-      startupEmoji: false, // true or false
+      startupCap: 100, // 1-200
+      startupVolume: 25, // 0-100
+      startupEmoji: true, // true or false
       autowoot: true,
-      autoskip: false,
+      autoskip: true,
       smartSkip: true,
       cmdDeletion: true,
-      maximumAfk: 120,
+      maximumAfk: 180,
       afkRemoval: true,
       maximumDc: 60,
       bouncerPlus: true,
@@ -70,7 +70,7 @@
       maximumCycletime: 10,
       voteSkip: false,
       voteSkipLimit: 10,
-      historySkip: false,
+      historySkip: true,
       timeGuard: true,
       maximumSongLength: 10,
       autodisable: true,
@@ -88,10 +88,10 @@
       ],
       afkpositionCheck: 15,
       afkRankCheck: "ambassador",
-      motdEnabled: false,
+      motdEnabled: True,
       motdInterval: 5,
-      motd: "Temporary Message of the Day",
-      filterChat: true,
+      motd: "♫ Enjoy Playing Unlimited Music With Havok ♫",
+      filterChat: false,
       etaRestriction: false,
       welcome: true,
       opLink: null,
